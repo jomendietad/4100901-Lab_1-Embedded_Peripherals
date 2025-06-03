121cc82199b5b6959a867ad303fb6c390499a50c
Johan Sebastian Mendieta Dilbert <jomendietad@unal.edu.co>
2025-06-03 14:00:47 -0500

    Se hace el repositorio para la realización del laboratorio 1

---
+++ build/Debug/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,8 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "4100901-Lab_1-Embedded_Peripherals.map"
+  )
+endif()
